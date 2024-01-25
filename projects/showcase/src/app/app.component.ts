58587aacbccab0ffe26982a58886db1e34d1d5b9
benb <benboswell@smplfinance.com>
2024-01-25 12:00:52 -0700

    commit

--- projects/showcase/src/app/app.component.ts
+++ projects/showcase/src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  SmplIdentityClientService
+} from "../../../smplecosystem/smpl-identity-client/src/lib/smpl-identity-client.service";
 
 
 @Component({
@@ -9,6 +12,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'showcase';
+
+  constructor(
+    private smplIdentityClientService: SmplIdentityClientService,
+  ) {}
+
+  async ngOnInit() {
+      await this.smplIdentityClientService.broadcastTransaction('cosmos1ph09ga9nvef89e8en7pr9xejvpk5wgmqzgu8u9');
+  }
 }
--- projects/smplecosystem/smpl-identity-client/src/lib/smpl-identity-client.service.ts
+++ projects/smplecosystem/smpl-identity-client/src/lib/smpl-identity-client.service.ts
@@ -11,19 +11,33 @@ export class SmplIdentityClientService {
 
   }
 
-  async getWallet() {
-    const mnemonic: string = 'measure sunny ritual tone local dice fame wood bar arm slam bean cricket spirit knife right join expose party axis fun reopen push oval';
-    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic);
+  async broadcastTransaction(creator: string) {
+    const mnemonic: string = 'security require mad bronze cabin name advance fee hidden arrest swift blast try valley midnight raccoon gym such grape fury try seven power orchard';
+    const walletAddressFromMnemonic = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic);
 
     const client = new Client({
-    apiURL: "http://localhost:1317",
-    prefix: "http://localhost:26657",
-    rpcURL: "cosmos"
-  },
-    wallet
-  );
-    const balances = await client.CosmosBankV1Beta1.query.queryAllBalances(
-      'cosmos1qvyjz6j4anqj40hxq3s5dgd67nlxldmwzvjf6j'
+        apiURL: "http://localhost:1317",
+        prefix: "cosmos",
+        rpcURL: "http://localhost:26657"
+      },
+      walletAddressFromMnemonic
     );
-  }
+
+    await client.SmplidentitychainDid.tx.sendMsgUpsertDid({
+      value: {
+        creator: creator,
+        didDocument: undefined,
+        didDocumentMetadata: undefined,
+        signature: 'billy'
+      },
+      fee: {
+        amount: [{ amount: '0', denom: 'stake' }],
+        gas: '200000',
+      },
+      memo: '',
+    }).then(result => console.log('tx result', result));
+    // return tx_result;
+    // const transactions = await client.SmplidentitychainDid.query.queryResolveDidRequest(creator);
+    // console.log('transactions', transactions)
+  };
 }
